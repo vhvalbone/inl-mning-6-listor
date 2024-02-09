2cf733494e9fe845e1815ccf1e9775b2d1e2c3e0
vhvalbone <albin06svensson@gmail.com>
2024-02-09 12:40:21 +0100

    Add project files.

---
+++ inlämning 6 listor/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Skriv in namn på matvaror:");
+        string[] matvaror = Console.ReadLine().Split();
+
+        Console.WriteLine("Skriv in matvarornas priser:");
+        int[] priser = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        Console.WriteLine("Skriv in din handlingslista:");
+        string[] handlingslista = Console.ReadLine().Split();
+
+        int totalKostnad = BeräknaKostnad(matvaror, priser, handlingslista);
+        Console.WriteLine($"Priset för handlingslistan är {totalKostnad}");
+    }
+
+    static int BeräknaKostnad(string[] matvaror, int[] priser, string[] handlingslista)
+    {
+        Dictionary<string, int> produktTillPris = new Dictionary<string, int>();
+
+        for (int i = 0; i < matvaror.Length; i++)
+        {
+            produktTillPris[matvaror[i]] = priser[i];
+        }
+
+        int totalKostnad = 0;
+        foreach (string produkt in handlingslista)
+        {
+            if (produktTillPris.ContainsKey(produkt))
+            {
+                totalKostnad += produktTillPris[produkt];
+            }
+        }
+
+        return totalKostnad;
+    }
+}
+
